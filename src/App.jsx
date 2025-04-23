e1ab85002120f74ef7603ee954bb560556d96f47
Pachecolosgroup <cesarppublicidad03@gmail.com>
2025-04-22 20:48:40 -0500

    smallchanges

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Navbar from "./components/Navbar"
 
 
 
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-    <h1 className="text-5xl caret-amber-300 text-center pt-2"> hola feo estamos testeando los cambios para ver </h1>
+    <Navbar />
       
     </>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { MenuIcon, XIcon } from '@heroicons/react/24/outline'
+
+
+
+export default function Navbar() {
+  return (
+
+    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+
+      <div className='px-2'>
+        <h1> testing elements </h1>
+
+      </div>
+
+      <h1> Dimelo papi</h1>
+
+    </div>
+  )
+}
